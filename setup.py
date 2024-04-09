0031bcdd0f3705c6bcc2682c6c92289861f92d28
The Disala <chamoddisala41@gmail.com>
2024-04-09 04:19:05 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,17 +6,17 @@ import os
 # Define the coordinates and use the `actions` list
 actions = [
     (266, 287, 3),  # unattended access
-    (681, 564, 2),  # next
-    (260, 507, 2),  # i accept agreement
-    (673, 567, 2),  # next
-    (672, 565, 2),  # next
-    (675, 566, 2),  # next
-    (677, 570, 2),  # install (wait 15sec)
-    (261, 306, 2),  # type pass
-    (671, 567, 2),  # next (wait 15sec)
-    (437, 303, 2),  # tic launch iperius
-    (667, 567, 2),  # finish
-    (447, 286, 2),  # copy id (launch iperius)
+    (681, 564, 3),  # next
+    (260, 507, 3),  # i accept agreement
+    (673, 567, 3),  # next
+    (672, 565, 3),  # next
+    (675, 566, 3),  # next
+    (677, 570, 3),  # install (wait 15sec)
+    (261, 306, 3),  # type pass
+    (671, 567, 3),  # next (wait 15sec)
+    (437, 303, 3),  # tic launch iperius
+    (667, 567, 3),  # finish
+    (447, 286, 3),  # copy id (launch iperius)
 ]
 
 # Wait for a few seconds to give time to focus on the target application
@@ -24,19 +24,19 @@ time.sleep(10)
 password = "TheDisa1a"
     
 for x, y, duration in actions:
-    if (x, y, duration) == (677, 570, 4):
+    if (x, y, duration) == (677, 570, 3):
         pag.click(x, y, duration=duration)
         time.sleep(15)
-    elif (x, y, duration) == (261, 306, 4):
+    elif (x, y, duration) == (261, 306, 3):
         pag.click(x, y, duration=duration)
         pag.typewrite(password)
-    elif (x, y, duration) == (671, 567, 4):
+    elif (x, y, duration) == (671, 567, 3):
         pag.click(x, y, duration=duration)
         time.sleep(15)
-    elif (x, y, duration) == (667, 567, 4):
+    elif (x, y, duration) == (667, 567, 3):
         pag.click(x, y, duration=duration)
         time.sleep(15)
-    elif (x, y, duration) == (447, 286, 4):
+    elif (x, y, duration) == (447, 286, 3):
         os.system('"C:\\Program Files\\IperiusRemote\\IperiusRemote.exe"')
         time.sleep(5)
         pag.click(x, y, duration=duration)
