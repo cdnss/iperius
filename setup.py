7345e1b088d7fb93d2d4db305bfaa41e882d094f
The Disala <chamoddisala41@gmail.com>
2024-04-09 04:12:48 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,19 +6,18 @@ import os
 
 # Define the coordinates and use the `actions` list
 actions = [
-    (266, 287, 4),  # unattended access
-    (681, 564, 4),  # next
-    (260, 507, 4),  # i accept agreement
-    (673, 567, 4),  # next
-    (672, 565, 4),  # next
-    (265, 299, 4),  # untic create desktop icon
-    (675, 566, 4),  # next
-    (677, 570, 4),  # install (wait 15sec)
-    (261, 306, 4),  # type pass
-    (671, 567, 4),  # next (wait 15sec)
-    (437, 303, 4),  # tic launch iperius
-    (667, 567, 4),  # finish
-    (447, 286, 4),  # copy id (launch iperius)
+    (266, 287, 3),  # unattended access
+    (681, 564, 2),  # next
+    (260, 507, 2),  # i accept agreement
+    (673, 567, 2),  # next
+    (672, 565, 2),  # next
+    (675, 566, 2),  # next
+    (677, 570, 2),  # install (wait 15sec)
+    (261, 306, 2),  # type pass
+    (671, 567, 2),  # next (wait 15sec)
+    (437, 303, 2),  # tic launch iperius
+    (667, 567, 2),  # finish
+    (447, 286, 2),  # copy id (launch iperius)
 ]
 
 # Wait for a few seconds to give time to focus on the target application
