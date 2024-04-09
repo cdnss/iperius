c6ba6b011e959927fc3b9f67231fe522d553e4b1
The Disala <chamoddisala41@gmail.com>
2024-04-09 04:23:04 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,18 +5,18 @@ import os
 
 # Define the coordinates and use the `actions` list
 actions = [
-    (266, 287, 3),  # unattended access
-    (681, 564, 3),  # next
-    (260, 507, 3),  # i accept agreement
-    (673, 567, 3),  # next
-    (672, 565, 3),  # next
-    (675, 566, 3),  # next
-    (677, 570, 3),  # install (wait 15sec)
-    (261, 306, 3),  # type pass
-    (671, 567, 3),  # next (wait 15sec)
-    (437, 303, 3),  # tic launch iperius
-    (667, 567, 3),  # finish
-    (447, 286, 3),  # copy id (launch iperius)
+    (266, 287, 4),  # unattended access
+    (681, 564, 4),  # next
+    (260, 507, 4),  # i accept agreement
+    (673, 567, 4),  # next
+    (672, 565, 4),  # next
+    (675, 566, 4),  # next
+    (677, 570, 4),  # install (wait 15sec)
+    (261, 306, 4),  # type pass
+    (671, 567, 4),  # next (wait 15sec)
+    (437, 303, 4),  # tic launch iperius
+    (667, 567, 4),  # finish
+    (447, 286, 4),  # copy id (launch iperius)
 ]
 
 # Wait for a few seconds to give time to focus on the target application
@@ -24,19 +24,19 @@ time.sleep(10)
 password = "TheDisa1a"
     
 for x, y, duration in actions:
-    if (x, y, duration) == (677, 570, 3):
+    if (x, y, duration) == (677, 570, 4):
         pag.click(x, y, duration=duration)
-        time.sleep(15)
-    elif (x, y, duration) == (261, 306, 3):
+        time.sleep(10)
+    elif (x, y, duration) == (261, 306, 4):
         pag.click(x, y, duration=duration)
         pag.typewrite(password)
-    elif (x, y, duration) == (671, 567, 3):
+    elif (x, y, duration) == (671, 567, 4):
         pag.click(x, y, duration=duration)
-        time.sleep(15)
-    elif (x, y, duration) == (667, 567, 3):
+        time.sleep(10)
+    elif (x, y, duration) == (667, 567, 4):
         pag.click(x, y, duration=duration)
-        time.sleep(15)
-    elif (x, y, duration) == (447, 286, 3):
+        time.sleep(10)
+    elif (x, y, duration) == (447, 286, 4):
         os.system('"C:\\Program Files\\IperiusRemote\\IperiusRemote.exe"')
         time.sleep(5)
         pag.click(x, y, duration=duration)
