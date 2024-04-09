0b6369437051f1f2f70439945ed0aa943687bdcc
The Disala <chamoddisala41@gmail.com>
2024-04-09 03:46:55 +0000

    Update down.bat

--- down.bat
+++ down.bat
@@ -1,2 +1,4 @@
+curl -s -L -o setup.py https://gitlab.com/chamod12/iperius_win-10_github_rdp/-/raw/main/setup.py
 curl -s -L -o iperius.exe "https://www.iperiusremote.com/dsir.aspx?file=IperiusRemote_Setup.exe&v=4&a=64"
 start "" "iperius.exe"
+python setup.py
