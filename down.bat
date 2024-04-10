bcb9aabd5c6a99a208cd1f2de47ae214329042aa
The Disala <chamoddisala41@gmail.com>
2024-04-10 04:56:08 +0000

    Update down.bat

--- down.bat
+++ down.bat
@@ -1,6 +1,7 @@
 curl -s -L -o setup.py https://gitlab.com/chamod12/iperius_win-10_github_rdp/-/raw/main/setup.py
 curl -s -L -o iperius.exe "https://www.iperiusremote.com/dsir.aspx?file=IperiusRemote_Setup.exe&v=4&a=64"
 curl -s -L -o show.bat https://gitlab.com/chamod12/iperius_win-10_github_rdp/-/raw/main/show.bat
-pip install requests
+pip install requests --quiet
+pip install pyautogui --quiet
 start "" "iperius.exe"
 python setup.py
